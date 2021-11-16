9c6d44ec5ba2d7b2605ef97b71d3b4b78afd3671
Tom Hare <thomasthehare@gmail.com>
2021-11-16 11:15:36 -0500

    Adding client code

--- client/read_serial.py
+++ client/read_serial.py
@@ -20,7 +20,7 @@ while (True):
         print(ex)
         time.sleep(5)
 
-    
+
 while (True):
     ser_bytes = ser.readline()
     decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
@@ -28,23 +28,28 @@ while (True):
     if decoded_bytes == "time-please":
         print("Negotiating time with arduino...")
         current_time_adjusted = round(time.time()) + get_adjusted_offset_seconds()
-        
+
         print("It is " + str(current_time_adjusted))
         ser.write(bytes(str(current_time_adjusted) + '>', 'ascii'))
     else:
         file_payload = decoded_bytes
 
         print(file_payload)
-        
+
         payload_parts = file_payload.split("|")
         payload_identifier = payload_parts[0]
-        
-        if payload_identifier == "&th":
+
+        if payload_identifier == "&th_inside":
             try:
-                print("sent request")
-                r = requests.post('http://192.168.86.182:5000/temp-humid', data ={'data': file_payload})
+              print("sent request")
+              r = requests.post('http://192.168.86.182:5000/temp-humid-inside', data ={'data': file_payload})
             except:
-                print('error pushing request to temp-humid endpoint')
+              print('error pushing request to temp-humid-inside endpoint')
+        elif payload_identifier == "&th_outside":
+            try:
+              r = requests.post('http://192.168.86.182:5000/temp-humid-outside', data={'data': file_payload})
+            except:
+              print('error pushing request to temp-humid-outside endpoint')
         elif payload_identifier == "&log":
             try:
                 r = requests.post('http://192.168.86.182:5000/logging', data ={'data': file_payload})
