736af97ac7191dc228fcb293cfd95e1ed43054d2
Tom Hare <thomasthehare@gmail.com>
2022-02-06 15:15:37 -0500

    doing alot not testing it

--- client/main.py
+++ client/main.py
@@ -2,11 +2,17 @@ import Adafruit_DHT
 from heat_index import calculate as heatindex
 import time
 import requests
+import RPi.GPIO as GPIO
+
+# TODO ID LIKE SOME LOGGING
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 
 PIN_TEMP_HUMIDITY_INSIDE = 4
-PIN_TEMP_HUMIDITY_OUTSIDE = -1
+PIN_TEMP_HUMIDITY_OUTSIDE = 4 #TODO NUH UH change this to real pin
+PIN_VALVE_POWER = -1
+PIN_SOIL_MOISTURE_POWER = -1
+PIN_SOIL_MOISTURE_DATA = -1
 
 SERVER_URL = 'http://192.168.86.182:5000'
 URL_TEMP_HUMID_INSIDE = '/temp-humid-inside'
@@ -35,13 +41,28 @@ class TempHumidReading:
 class WebClient:
 
     def format_data_string(self, reading: TempHumidReading):
-         data = "th_inside|{timestamp}|humidity:{h}|temp:{t}|heat-index:{hi}".format(timestamp=timestamp(), h=reading.get_humidity(), t=reading.get_temp(), hi=reading.get_heat_index())
+         return "not_relevant|{timestamp}|humidity:{h}|temp:{t}|heat-index:{hi}".format(timestamp=timestamp(), h=reading.get_humidity(), t=reading.get_temp(), hi=reading.get_heat_index())
 
     def send_temp_humidity_reading(self, reading: TempHumidReading, url: str):
-        #r = requests.post(url, data ={'data': self.format_data_string(reading)})
-
+        data = self.format_data_string(reading)
         print(data)
-        #r = requests.post(SERVER_URL + '/temp-humid-inside', data ={'data': data})
+        r = requests.post(url, data ={'data': data})
+
+class SoilMoistureSensor:
+    def __init__(self, data_pin, power_pin):
+        self.data_pin = data_pin
+        self.power_pin = power_pin
+        GPIO.setup(self.power_pin, GPIO.OUT)
+        GPIO.setup(self.data_pin, GPIO.IN)
+
+    def read(self) -> float:
+        # No idea, will return a value though
+        # givee it power
+        # wait a bit
+        # take data reading
+        # turn off the power
+
+        return .9 #total guess at a high number
 
 class TempHumidSensor:
 
@@ -63,7 +84,7 @@ class TempHumidSensor:
 
 class TempHumidLogTask:
 
-    def __init__(self, run_every_seconds: int, sensor: TempHumiditySensor, url: str, web_client: WebClient):
+    def __init__(self, run_every_seconds: int, sensor: TempHumidSensor, url: str, web_client: WebClient):
         self.run_every_seconds = run_every_seconds
         self.sensor = sensor
         self.last_run_ts = 0
@@ -75,6 +96,7 @@ class TempHumidLogTask:
 
     def run(self) -> bool:
         if not self.should_run():
+            print('task not ready yet')
             return False
 
         reading = self.sensor.read()
@@ -83,12 +105,85 @@ class TempHumidLogTask:
 
         return True
 
+class Valve:
+    def __init__(self, signal_pin, open_duration):
+        self.last_opened_time = 0
+        self.open_duration = open_duration
+        self.is_open = False
+        self.signal_pin = signal_pin
+
+        GPIO.setup(self.signal_pin, GPIO.OUT)
+
+    def open(self):
+        GPIO.output(self.signal_pin, GPIO.HIGH)
+        self.is_open = True
+        self.last_opened_time = timestamp()
+
+    def close(self):
+        GPIO.output(self.signal_pin, GPIO.LOW)
+        self.is_open = False
+
+class ValveCloseTask:
+    def __init__(self, valve: Valve):
+        self.valve = valve
+
+    def run(self):
+        if self.valve.is_open and timestamp() > (self.valve.last_opened_time + self.valve.open_duration):
+            self.valve.close()
+            return True
+
+        return False
+
+class WaterPlantTask:
+    def __init__(self, run_every_seconds, valve: Valve, soil_moisture_sensor: SoilMoistureSensor, soil_moisture_threshold: float):
+        self.valve = valve
+        self.soil_moisture_sensor = soil_moisture_sensor
+        self.last_opened_timestamp = 0
+        self.soil_moisture_threshold = soil_moisture_threshold
+        self.cooldown_seconds = 300
+        self.run_every_seconds = run_every_seconds
+        self.last_evaluated_timestamp = 0
+
+    def should_run(self) -> bool:
+        if timestamp() < (this.last_evaluated_timestamp + self.run_every_seconds):
+            return False
+
+        self.last_evaluated_timestamp = timestamp()
+
+        soil_moisture_reading = self.soil_moisture_sensor.read()
+        if soil_moisture_reading < this.soild_moisture_threshold:
+            if (self.last_opened_timestamp + self.cooldown_seconds) < timestamp():
+                return True
+
+        return False
+
+    def run(self):
+        if not self.should_run():
+            return False
+        
+        self.valve.open()
+        return True
+
+# use board numbers vs some lower level option
+GPIO.setmode(GPIO.BOARD)
 
 tempHumidInside = TempHumidSensor(PIN_TEMP_HUMIDITY_INSIDE)
 tempHumidOutside = TempHumidSensor(PIN_TEMP_HUMIDITY_OUTSIDE)
 web_client = WebClient()
+soil_moisture_sensor = SoilMoistureSensor(PIN_SOIL_MOISTURE_DATA, PIN_SOIL_MOISURE_POWER)
+valve = Valve(PIN_VALVE_POWER, 30)
 
 tasks = [
-    TempHumidLogTask(300, tempHumidInside, SERVER_URL + URL_TEMP_HUMIDITY_INSIDE, web_client), 
-    TempHumidLogTask(300, tempHumidOutside, SERVER_URL + URL_TEMP_HUMIDITY_OUTSIDE, web_client),
+    TempHumidLogTask(10, tempHumidInside, SERVER_URL + URL_TEMP_HUMID_INSIDE, web_client), 
+    TempHumidLogTask(15, tempHumidOutside, SERVER_URL + URL_TEMP_HUMID_OUTSIDE, web_client),
+    WaterPlantTask(600, valve, soil_moisture_sensor, .8),
 ]
+
+while True:
+    for task in tasks:
+        try:
+            task.run();
+        except Exception as e:
+            print(e)
+    
+    time.sleep(5);
