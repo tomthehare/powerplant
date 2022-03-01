b4fb0e85d50c0033cb3dd2124892263f8f11c92a
Tom Hare <thomasthehare@gmail.com>
2022-03-01 11:27:00 -0500

    Adding changes to arduino code

--- server/arduino/PowerplantClient.ino
+++ server/arduino/PowerplantClient.ino
@@ -1,21 +1,25 @@
-#include <Adafruit_Sensor.h>
 #include <stdint.h>
-#include <DHT.h>
-#include <DHT_U.h>
 #include <stdlib.h>
 
-#include <Adafruit_Sensor.h>
+/**
+ * PIN ALLOCATIONS
+ */
+#define POWER_TO_SENSORS_SWITCH_PIN 13
+#define SELECTPIN_A 2
+#define SELECTPIN_B 3
+#define SELECTPIN_C 4
+#define SELECTPIN_D 5
+#define MUX_ENABLE 7
 
+void resetSoilMoisturePins() {
+  digitalWrite(POWER_TO_SENSORS_SWITCH_PIN, HIGH); // High = off
+  digitalWrite(SELECTPIN_A, LOW);
+  digitalWrite(SELECTPIN_B, LOW);
+  digitalWrite(SELECTPIN_C, LOW);
+  digitalWrite(SELECTPIN_D, LOW);
+  digitalWrite(MUX_ENABLE, HIGH);
+}
 
-#define DHTPIN_INSIDE 2
-#define DHTPIN_OUTSIDE 4
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-
-DHT dht_inside(DHTPIN_INSIDE, DHTTYPE);
-DHT dht_outside(DHTPIN_OUTSIDE, DHTTYPE);
-
-#define PIN_WATERVALVE 7
-#define VALVE_COOLDOWN_SECONDS 60
 
 class OneShotScheduler {
   private:
@@ -106,65 +110,6 @@ class TimeCoordinator {
     }
 };
 
-class TempHumidityTask {
-  private:
-    OneShotScheduler *scheduler;
-    TimeCoordinator *tc;
-    float lastTempReading;
-    DHT *dht;
-    String tag;
-
-  public:
-    TempHumidityTask(OneShotScheduler *scheduler, TimeCoordinator *tc, DHT *dht, String tag) {
-      this->scheduler = scheduler;
-      this->tc = tc;
-      this->dht = dht;
-      this->tag = tag;
-    }
-  
-    float getLastTempReading() {
-      return this->lastTempReading;
-    }
-
-    String readSensor() {    
-      if (!this->scheduler->shouldRunTask()) {
-        return "";
-      }
-    
-      // Reading temperature or humidity takes about 250 milliseconds!
-      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-      float h = this->dht->readHumidity();
-   
-      // Read temperature as Fahrenheit (isFahrenheit = true)
-      float f = this->dht->readTemperature(true);
-      this->lastTempReading = f;
-  
-      // Check if any reads failed and exit early (to try again).
-      if (isnan(h) || isnan(f)) {
-        return "&err|Failed to read temp-humid sensor.";
-      }
-  
-      // Compute heat index in Fahrenheit (the default)
-      float hif = this->dht->computeHeatIndex(f, h);
-
-      this->scheduler->markTaskAsRun();
-  
-      String str = "&th_";
-      str.concat(this->tag);
-      str.concat("|");
-      str.concat(this->tc->getCurrentTimeStamp());
-      str.concat("|humidity:");
-      str.concat(h);
-      str.concat("|");
-      str.concat("temp-fht:");
-      str.concat(f);
-      str.concat("|");
-      str.concat("heat-index-fht:");
-      str.concat(hif);
-  
-      return str;
-    }
-};
 
 class SerialWriter {
 
@@ -201,131 +146,152 @@ class Logger {
       prefix.concat(aString);
       sw->writeString(prefix);
     }
-};
+};   
 
-class ValveOperator {
+class SoilMoistureSensor {
   private:
-    TimeCoordinator *tc;
-    Logger *logr;
-    int secondsOpen;
+    int lastReadVoltage;
     OneShotScheduler *scheduler;
-    int lastDayOffsetRun8;
-    int lastDayOffsetRun12;
-    long valveOpenedAtTimestamp;
-    bool valveOpen;
-    TempHumidityTask *tempHumid;
- 
+    int dataPin;
+    long lastReadMillis;
+    String tag;
+    TimeCoordinator *tc;
+    Logger *logger;
+
+  /**
+   * signalPins indicate which pins to activate to read the proper sensor.
+   */
   public:
-    ValveOperator(Logger *logr, TimeCoordinator *tc, int secondsOpen, OneShotScheduler *scheduler, TempHumidityTask *tempHumid) {
-      this->secondsOpen = secondsOpen;
-      this->logr = logr;
-      this->tc = tc;
+    SoilMoistureSensor(String tag, OneShotScheduler *scheduler, int dataPin, TimeCoordinator *tc, Logger *logger) {
+      this->lastReadVoltage = -1;
       this->scheduler = scheduler;
-      this->lastDayOffsetRun8 = 0;
-      this->lastDayOffsetRun12 = 0;
-      this->tempHumid = tempHumid;
+      this->dataPin = dataPin;
+      this->lastReadMillis = -1;
+      this->tag = tag;
+      this->tc = tc;
+      this->logger = logger;
     }
 
-    void evaluateSchedule() {
+    String readSensor() {
       if (!this->scheduler->shouldRunTask()) {
-        return;
+        return "";
       }
 
       this->scheduler->markTaskAsRun();
 
-      if (this->valveOpen) {
-        if (this->tc->getCurrentTimeStamp() > (this->valveOpenedAtTimestamp + this->secondsOpen)) {
-          this->closeValve();
-        } else {
-          // The valve is open and doesnt need to be closed yet.  We can evaluate again soon.
-          return;
-        }
-      }
-
-      int currentDay = this->tc->getCurrentDayOffset();
-      int currentHour = this->tc->getCurrentHourOffset();
-      int currentMinute = this->tc->getCurrentMinuteOffset();
-
-      // Do the 8am check
-      if (currentDay != this->lastDayOffsetRun8 && this->tc->getCurrentHourOffset() == 8) {
-        this->openValve();
-	      this->lastDayOffsetRun8 = this->tc->getCurrentDayOffset();
-      }
-
-      if (currentDay != this->lastDayOffsetRun12 && this->tc->getCurrentHourOffset() == 12) {
-        
-        if (this->tempHumid->getLastTempReading() > 80) {
-          this->openValve();
-  	      String logString = "Did the 12pm check and determined that it is plenty hot: ";
-          logString.concat(this->tempHumid->getLastTempReading());
-  	      this->logr->doLog(logString);
-        } else {
-          String logString = "Did the 12pm check but determined not hot enough today: ";
-          logString.concat(this->tempHumid->getLastTempReading());
-  	      this->logr->doLog(logString);
-        }
-        this->lastDayOffsetRun12 = this->tc->getCurrentDayOffset();
-      }
-    }
-
-    void openValve() {
-      digitalWrite(PIN_WATERVALVE, HIGH);
-      this->valveOpen = true;
-      this->valveOpenedAtTimestamp = this->tc->getCurrentTimeStamp();
+      digitalWrite(MUX_ENABLE, LOW);
+      this->logger->doLog("MUX_ENABLE set to LOW");
       
-      this->logr->doLog("Opened valve");
+      digitalWrite(POWER_TO_SENSORS_SWITCH_PIN, LOW);
+      this->logger->doLog("POWER_TO_SENSORS_SWITCH_PIN set to LOW");
+
+      // 0001
+      if ((this->dataPin & 1) > 0) {
+        digitalWrite(SELECTPIN_A, HIGH);
+        this->logger->doLog("SEL0 set to HIGH");
+      }
+
+      // 0010
+      if ((this->dataPin & 2) > 0) {
+        digitalWrite(SELECTPIN_B, HIGH);
+        this->logger->doLog("SEL1 set to HIGH"); 
+      }
+
+      // 0100
+      if ((this->dataPin & 4) > 0) {
+        digitalWrite(SELECTPIN_B, HIGH);
+        this->logger->doLog("SEL2 set to HIGH"); 
+      }
+
+      // 1000
+      if ((this->dataPin & 8) > 0) {
+        digitalWrite(SELECTPIN_B, HIGH);
+        this->logger->doLog("SEL3 set to HIGH"); 
+      }
+
+      delay(1000);
+
+      this->lastReadVoltage = analogRead(A0);
+      this->lastReadMillis = this->tc->getCurrentTimeStamp();
+      
+      this->resetSoilMoisturePins();
+
+      String str = "&sm|";
+      str.concat(this->tc->getCurrentTimeStamp());
+      str.concat("|");
+      str.concat(this->tag);
+      str.concat("|");
+      str.concat(this->lastReadVoltage);
+
+      return str;
     }
 
-    void closeValve() {
-      digitalWrite(PIN_WATERVALVE, LOW);
-      this->valveOpen = false;
-      this->valveOpenedAtTimestamp = 0;
-      this->logr->doLog("Closed valve");
+    void resetSoilMoisturePins() {
+      this->logger->doLog("Shutting down after read..");
+      digitalWrite(POWER_TO_SENSORS_SWITCH_PIN, HIGH); // High = off
+      this->logger->doLog("POWER_TO_SENSORS_SWITCH_PIN set to HIGH");
+      digitalWrite(SELECTPIN_A, LOW);
+      this->logger->doLog("SEL0 set to LOW");
+      digitalWrite(SELECTPIN_B, LOW);
+      this->logger->doLog("SEL1 set to LOW");
+      digitalWrite(SELECTPIN_C, LOW);
+      this->logger->doLog("SEL2 set to LOW");
+      digitalWrite(SELECTPIN_D, LOW);
+      this->logger->doLog("SEL3 set to LOW");
     }
 };
-
 
 SerialWriter serialWriter;
 TimeCoordinator timeKeeper;
 Logger logr(&serialWriter, &timeKeeper);
-OneShotScheduler tempHumidSchedulerInside(60000);
-OneShotScheduler tempHumidSchedulerOutside(60000);
-TempHumidityTask tempHumidityInside(&tempHumidSchedulerInside, &timeKeeper, &dht_inside, "inside");
-TempHumidityTask tempHumidityOutside(&tempHumidSchedulerOutside, &timeKeeper, &dht_outside, "outside");
-OneShotScheduler mainWaterValveScheduler(10000); // evaluate if it should run every n milliseconds, this does not mean it will run, just that it will have a chance to evaluate.
-ValveOperator mainWaterValve(&logr, &timeKeeper, 50, &mainWaterValveScheduler, &tempHumidityInside);
 
-void setup() {
+OneShotScheduler limeSensorScheduler0(15000);
+SoilMoistureSensor limeSoilMoisureSensor0("lime-soil", &limeSensorScheduler0, 0, &timeKeeper, &logr);
+
+OneShotScheduler limeSensorScheduler1(15000);
+SoilMoistureSensor limeSoilMoisureSensor1("lime-air", &limeSensorScheduler1, 1, &timeKeeper, &logr);
+
+
+void setup() { 
   Serial.begin(9600);
 
-  pinMode(PIN_WATERVALVE, OUTPUT); 
+  pinMode(POWER_TO_SENSORS_SWITCH_PIN, OUTPUT);
+  pinMode(SELECTPIN_A, OUTPUT);
+  pinMode(SELECTPIN_B, OUTPUT);
+  pinMode(SELECTPIN_C, OUTPUT);
+  pinMode(SELECTPIN_D, OUTPUT);
+  pinMode(MUX_ENABLE, OUTPUT);
 
-  // Start(?) the temp/humdity chip
-  dht_inside.begin();
-  dht_outside.begin();
+  digitalWrite(POWER_TO_SENSORS_SWITCH_PIN, HIGH);
+  digitalWrite(SELECTPIN_A, LOW);
+  digitalWrite(SELECTPIN_B, LOW);
+  digitalWrite(SELECTPIN_C, LOW);
+  digitalWrite(SELECTPIN_D, LOW);
+  digitalWrite(MUX_ENABLE, HIGH);
 
   // Solicit the real world time in order to schedule water delivery properly.
   timeKeeper.coordinateRealWorldTime();
-  String logMessage = "Hello- the build is build A.  Arduino time synced: ";
+  String logMessage = "Starting up... Arduino time synced: ";
   logMessage.concat(String(timeKeeper.getCurrentTimeStamp()));
   logr.doLog(logMessage);
 }
 
 void loop() {
-  delay(100);
+  delay(3000);
 
-  String tempHumidInside = tempHumidityInside.readSensor();
+  logr.doLog("Reading input sensor 0");
+  String sensor0Reading = limeSoilMoisureSensor0.readSensor();
 
-  if (tempHumidInside.length() > 0) {
-    serialWriter.writeString(tempHumidInside);
+  if (sensor0Reading.length() > 0) {
+    serialWriter.writeString(sensor0Reading); 
   }
 
-  String tempHumidOutside = tempHumidityOutside.readSensor();
+  delay(3000);
 
-  if (tempHumidOutside.length() > 0) {
-    serialWriter.writeString(tempHumidOutside);
+  logr.doLog("Reading input sensor 1");
+  String sensor1Reading = limeSoilMoisureSensor1.readSensor();
+
+  if (sensor1Reading.length() > 0) {
+    serialWriter.writeString(sensor1Reading); 
   }
-
-  // No valve attached, commenting out for now.
-  // mainWaterValve.evaluateSchedule();
 }
